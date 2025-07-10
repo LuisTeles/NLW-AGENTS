2f7931579b1552c0dcee4cdb8917de95c27439ec
Luis Teles <devzluis@gmail.com>
2025-07-10 13:57:21 -0300

    final project base

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,17 @@
-import { fastifyCors } from '@fastify/cors';
-import { fastify } from 'fastify';
+import { fastifyCors } from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
+import { fastify } from 'fastify'
 import {
     serializerCompiler,
     validatorCompiler,
     type ZodTypeProvider,
-} from 'fastify-type-provider-zod';
-import { env } from './env.ts';
-import { createQuestionRoute } from './http/routes/create-question.ts';
-import { createRoomRoute } from './http/routes/create-room.ts';
-import { getRoomQuestions } from './http/routes/get-room-questions.ts';
-import { getRoomsRoute } from './http/routes/get-rooms.ts';
+} from 'fastify-type-provider-zod'
+import { env } from './env.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts'
+import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomQuestions } from './http/routes/get-room-questions.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { uploadAudioRoute } from './http/routes/upload-audio.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -17,18 +19,19 @@ app.register(fastifyCors, {
     origin: 'http://localhost:5173',
 })
 
+app.register(fastifyMultipart)
+
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
 app.get('/health', () => {
-    return { status: 'ok' }
+    return 'OK'
 })
 
 app.register(getRoomsRoute)
 app.register(createRoomRoute)
 app.register(getRoomQuestions)
 app.register(createQuestionRoute)
+app.register(uploadAudioRoute)
 
 app.listen({ port: env.PORT })
-
-
